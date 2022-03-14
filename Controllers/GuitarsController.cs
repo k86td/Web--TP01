ff8671576b78091544acdb99f94c38820dc36eee
jouelelt310 <justin_ouellet@hotmail.com>
2022-03-14 19:50:11 -0400

    Revert "GuitarForm.cd added + DAL actions"
    This reverts commit 53fd44f23cca923e8a9fb150db953ef56bb76f23.

--- Controllers/GuitarsController.cs
+++ Controllers/GuitarsController.cs
@@ -37,50 +37,6 @@ namespace TP01.Controllers
             return RedirectToAction("Index");
         }
 
-        public ActionResult Creedit(int? id)
-        {
-            if (id.HasValue)
-            {
-                Guitar guitar = DB.Guitars.Find(id);
-                if (guitar == null) { return PartialView("_error"); }
-                return View(guitar);
-            }
-            else
-            {
-                return View(new Guitar());
-            }
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Creedit(Guitar guitar)
-        {
-            int? id = guitar.Id;
-            if (id.HasValue)
-            {
-                if (ModelState.IsValid)
-                {
-                    DB.ModifierGuitar(guitar);
-                }
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                if (ModelState.IsValid)
-                {
-
-                    DB.AjouterGuitar(guitar);
-                }
-                return RedirectToAction("Index");
-            }
-        }
-
-        
-
-        
-
-
-
 
     }
 }
